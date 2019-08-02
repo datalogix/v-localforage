38c012b125fb55646caa13356d68c361eab081ec
Ricardo Gobbo de Souza <ricardo@datalogix.com.br>
2019-08-02 17:30:12 -0300

    chore: update dist files

--- dist/vue-localforage.umd.js
+++ dist/vue-localforage.umd.js
@@ -10,25 +10,9 @@
 		throw new Error('Dynamic requires are not currently supported by rollup-plugin-commonjs');
 	}
 
-	function unwrapExports (x) {
-		return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
-	}
-
 	function createCommonjsModule(fn, module) {
 		return module = { exports: {} }, fn(module, module.exports), module.exports;
 	}
-
-	function getCjsExportFromNamespace (n) {
-		return n && n['default'] || n;
-	}
-
-	var commonjsHelpers = /*#__PURE__*/Object.freeze({
-		commonjsGlobal: commonjsGlobal,
-		commonjsRequire: commonjsRequire,
-		unwrapExports: unwrapExports,
-		createCommonjsModule: createCommonjsModule,
-		getCjsExportFromNamespace: getCjsExportFromNamespace
-	});
 
 	var localforage = createCommonjsModule(function (module, exports) {
 	/*!
